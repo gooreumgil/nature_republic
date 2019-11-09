5e08180d02f5130c084b589f73393ca6cc85d643
Gwanseok Lee <hunter407@naver.com>
2019-11-09 14:16:33 +0900

    first commit

---
+++ src/main/java/com/daeboo/naturerepublic/NaturerepublicApplication.java
@@ -0,0 +1,13 @@
+package com.daeboo.naturerepublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturerepublicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaturerepublicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daeboo/naturerepublic/NaturerepublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daeboo.naturerepublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturerepublicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
